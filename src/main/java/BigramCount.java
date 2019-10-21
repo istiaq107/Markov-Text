dba35a8a5775efc19e9ad71f2c7126ad65c2b531
Mohammed Istiaq Ullah <miullah@uwaterloo.ca>
2019-10-21 18:22:59 -0400

    BigramCount updated

--- src/main/java/BigramCount.java
+++ src/main/java/BigramCount.java
@@ -1,22 +1,7 @@
-/**
- * Bespin: reference implementations of "big data" algorithms
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
-package io.bespin.java.mapreduce.bigram;
-
-import io.bespin.java.util.Tokenizer;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -37,13 +22,6 @@ import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 import org.kohsuke.args4j.ParserProperties;
 
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-
-/**
- * Counts bigrams.
- */
 public class BigramCount extends Configured implements Tool {
   private static final Logger LOG = Logger.getLogger(BigramCount.class);
 
@@ -54,10 +32,10 @@ public class BigramCount extends Configured implements Tool {
     @Override
     public void map(LongWritable key, Text value, Context context)
         throws IOException, InterruptedException {
-      List<String> tokens = Tokenizer.tokenize(value.toString());
+      StringTokenizer tokenizer = StringTokenizer(value.toString());
+      int tokenCount = tokenizer.countTokens();
 
-      if (tokens.size() < 2) return;
-      for (int i = 1; i < tokens.size(); i++) {
+      for (int i = 1; i < tokenCount; i++) {
         BIGRAM.set(tokens.get(i - 1) + " " + tokens.get(i));
         context.write(BIGRAM, ONE);
       }
